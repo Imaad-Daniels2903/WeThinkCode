8ed8a937f9adbb89cc4be454e42d3a8f8e58cd34
Sadiek Daniels <sadiek@atcompsolutions.co.za>
2025-07-10 21:57:37 +0200

    Editted outdated.py

--- Problem Set 3/outdated/outdated.py
+++ Problem Set 3/outdated/outdated.py
@@ -4,7 +4,13 @@ MONTHS = {
     "February" : "02",
     "March" : "03",
     "April" : "04",
-    "May" : "0: "11",
+    "May" : "05",
+    "June" : "06",
+    "July" : "07",
+    "August" : "08",
+    "September" : "09",
+    "October" : "10",
+    "November" : "11",
     "December" : "12"
 }
 
@@ -13,7 +19,7 @@ def main():
         try:
             date = input("Date: ").strip().title()
             date_list = date.split()
-            if not date_list[0] in MONTHS and (len(date_list) > 1 and  not date_list[0].isalpha()):
+            if not date_list[0] in MONTHS and (len(date_list) > 1 and not date_list[0].isalpha()):
                 raise ValueError()
 
         except ValueError:
@@ -21,37 +27,47 @@ def main():
 
         else:
             if len(date_list) == 1:
-                output_list = []
-                output = date_list[0].split("/")
-                for x in output:
-                    if len(x) == 1:
-                        output_list.append(f"0{x}")
+                if date_list[0].split("/")[0].isalpha() or int(date_list[0].split("/")[0]) > 12 or int(date_list[0].split("/")[1]) > 31:
+                    pass
+                else:
+                    output_list = []
+                    output = date_list[0].split("/")
+                    for x in output:
+                        if len(x) == 1:
+                            output_list.append(f"0{x}")
 
-                    elif len(x) == 4:
-                        output_list.insert(0, x)
+                        elif len(x) == 4:
+                            output_list.insert(0, x)
 
-                    else:
-                        output_list.append(x)
-                print("-".join(output_list))
-                break
+                        else:
+                            output_list.append(x)
+                    print("-".join(output_list))
+                    break
 
             else:
-                output_list = []
-                for x in date_list:
-                    if x in MONTHS:
-                        output_list.append(MONTHS[x])
-
-                    elif len(x) == 1 or len(x) == 2 and "," in x:
-                        output_list.append(f"0{x[0]}")
-
-                    elif len(x) == 4:
-                        output_list.insert(0, x)
-
+                if "," in date_list[1]:
+                    if int(date_list[1][0:date_list[1].index(",")]) > 31:
+                        pass
                     else:
-                        output_list.append(x)
+                        output_list = []
+                        for x in date_list:
+                            if x in MONTHS:
+                                output_list.append(MONTHS[x])
 
-                print("-".join(output_list))
-                break
+                            elif len(x) == 1 or len(x) == 2 and "," in x:
+                                output_list.append(f"0{x[0]}")
+
+                            elif len(x) == 4:
+                                output_list.insert(0, x)
+
+                            else:
+                                output_list.append(x)
+
+                        print("-".join(output_list))
+                        break
+                else:
+                    pass
+
 
 
 if __name__ == "__main__":
