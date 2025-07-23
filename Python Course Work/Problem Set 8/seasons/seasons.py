6bd7619f9104ee5a4ae4918d0db732fe8841ccca
Sadiek Daniels <sadiek@atcompsolutions.co.za>
2025-07-23 14:14:22 +0200

    added problem set 8

--- Python Course Work/Problem Set 8/seasons/seasons.py
+++ Python Course Work/Problem Set 8/seasons/seasons.py
@@ -4,20 +4,21 @@ import sys
 
 
 def main():
+    today = date.today()
     try:
-        print(get_minutes(input("Date of Birth: ")))
+        print(get_minutes(input("Date of Birth: "), today))
 
     except ValueError:
         sys.exit("Invalid date")
 
 
-def get_minutes(dob):
+def get_minutes(dob, today):
     p = inflect.engine()
     user_dob = date.fromisoformat(dob.strip())
-    today = date.today()
     total_days = (today - user_dob).days
     total_minutes = total_days * 24 * 60
-    return f"{p.number_to_words(total_minutes, andword="")} minutes".capitalize()
+
+    return f"{p.number_to_words(total_minutes, andword='')} minutes".capitalize()
 
 
 
